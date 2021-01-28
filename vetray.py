9f58cf812ef702d23a9456a71befba0324baebfc
Max Pereira <max@pereira.at>
2021-01-28 12:44:56 -0800

    Added Refresh option to refresh power status and device names

--- vetray.py
+++ vetray.py
@@ -3,20 +3,26 @@ from pystray import Icon as icon, Menu as menu, MenuItem as item
 from PIL import Image
 import json
 
+# Rename account-ex.json to account.json and fill in your VeSync account details
 with open('account.json') as j:
   account = json.load(j)
-
-# Rename account-ex.json to account.json and fill in your VeSync account details
-manager = VeSync(account['email'], account['password'], time_zone=account['timezone'])
-manager.login()
-manager.update()
 
 dd = []
 de = []
 
-for device in manager.outlets:
-    dd.append(device.displayJSON()['Device Name'])
-    de.append(device.displayJSON()['Status'])
+def refreshList():
+    manager = VeSync(account['email'], account['password'], time_zone=account['timezone'])
+    manager.login()
+    manager.update()
+    firstRun = 0
+    dd.clear()
+    de.clear()
+    for device in manager.outlets:
+        dd.append(device.displayJSON()['Device Name'])
+        de.append(device.displayJSON()['Status'])
+    return manager
+
+manager = refreshList()
   
 def toggle(x):
     def inner(icon, item):
@@ -32,6 +38,7 @@ menuObj=menu(lambda: [
     item(dd[i],toggle(i))
     for i in range(0,len(dd))]
     + [menu.SEPARATOR]
+    + [item('Refresh Devices', lambda icon, item: refreshList())]
     + [item('Exit', lambda icon, item: icon.stop())]
 )
 
